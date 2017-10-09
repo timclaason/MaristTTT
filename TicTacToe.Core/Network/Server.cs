e6a1a123105eee5c2cdb90884d2f1daf773b8429
timclaason <timclaason@hotmail.com>
2017-10-09 14:48:24 -0500

    Added serverapplication type Info

---
+++ TicTacToe.Core/Network/Server.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using TicTacToe.Core.Structures;
+using TicTacToe.Core.Network.Servers;
+
+namespace TicTacToe.Core.Network
+{
+    public class Server : NetworkNode
+    {
+        private ServerApplication performClientHandshake(Socket socket)
+        {            
+            ///Client sends <TICTACTOE>
+            string gameInitiationMessage = ListenForMessage(socket);
+            if (gameInitiationMessage == NetworkMessages.TICTACTOE_REQUEST_TEXT)
+                return ServerApplication.TicTacToe;
+
+            return ServerApplication.Invalid;
+        }
+
+        private void spinupServerThread(Socket socket)
+        {
+            BackgroundWorker bg = new BackgroundWorker();
+
+            bg.DoWork += (sender, e2) =>
+                {
+                    try
+                    {
+                        ServerApplication selectedServer = performClientHandshake(socket);
+
+                        if (selectedServer == ServerApplication.Invalid)
+                        {
+                            this.CloseSocketConnection(socket, NetworkMessages.CLOSING_SOCKET_TEXT);
+                        }
+
+                        if(selectedServer == ServerApplication.TicTacToe)
+                        {
+                            TicTacToeServer tttServer = new TicTacToeServer();
+                            tttServer.CloneHandlers(this, tttServer);
+                            selectedServer = tttServer.PerformHandshake(socket);
+
+                            if(selectedServer == ServerApplication.TicTacToe)
+                                tttServer.Start(socket);
+                        }
+
+                        socket.Shutdown(SocketShutdown.Both);
+                        socket.Close();
+                    }
+                    catch
+                    {
+                        try
+                        {
+                            socket.Shutdown(SocketShutdown.Both);
+                            socket.Close();
+                        }
+                        catch { }
+                        return;
+                    }
+                };
+            bg.RunWorkerAsync();
+
+        }
+        
+        public void Start()
+        {
+            Socket listener = base.GetListener(Settings.LISTENING_PORT);
+
+            try
+            {
+                listener.Listen(10);
+
+                // Start listening for connections.
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a connection...");
+                    // Program is suspended while waiting for an incoming connection.
+
+                    Socket socket = base.ListenForConnection(listener);
+                    spinupServerThread(socket);                    
+                }
+
+            }
+            catch (Exception e)
+            {
+                ServerLogger.WriteToLog("ERR", e.Message);
+                throw;
+            }
+        }        
+    }
+
+    /// <summary>
+    /// Types of servers
+    /// </summary>
+    public enum ServerApplication
+    {
+        TicTacToe,
+        Info,
+        Invalid
+    }
+}
